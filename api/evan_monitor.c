3a96120c38f3736f632aed544f2f75a0a4b89aaf
Jukka Pietarinen <jukka.pietarinen@mrf.fi>
2020-01-13 12:57:23 +0200

    Fixed evan_monitor, sleep(1) caused loss of events.

--- api/evan_monitor.c
+++ api/evan_monitor.c
@@ -50,13 +50,13 @@ int main(int argc, char *argv[])
 	  ts = ((long long) evan.TimestampHigh << 32) +
 	    (long long) evan.TimestampLow;
 	  sec = ts / (EVG_RF_FREQ/EVG_RF_DIVIDER);
-	  /*	  if ((evan.EventCode & 0x0ff) != 0x7e) */
+	  if ((evan.EventCode & 0x0ff) != 0x7e)
 	    printf("Timestamp %08x%08x, %16.9Lf, event %02x, dbus %02x\n",
 		   evan.TimestampHigh, evan.TimestampLow, sec, 
 		   evan.EventCode & 0x00ff, evan.EventCode >> 8);
 	}
       /* Sleep for a while if there are no events in the event analyzer fifo */
-      sleep(1);
+      usleep(10000);
     }
 
   EvgEvanEnable(pEg, 0);
