1db714cdb9029a2590c3b756f9843c28b858d81f
Jukka Pietarinen <jukka.pietarinen@mrf.fi>
2021-06-29 09:02:39 +0300

    Added file wrapper/EvrSetCMLMode.c

---
+++ wrapper/EvrSetCMLMode.c
@@ -0,0 +1,47 @@
+#include <stdint.h>
+#include <endian.h>
+#include <byteswap.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/ioctl.h>
+#include <signal.h>
+#include "../api/erapi.h"
+
+int main(int argc, char *argv[])
+{
+  struct MrfErRegs *pEr;
+  int              fdEr;
+  int              ch, i;
+
+  if (argc < 2)
+    {
+      printf("Usage: %s /dev/era3 <channel> [<mode>]\n", argv[0]);
+      printf("Assuming: /dev/era3\n");
+      argv[1] = "/dev/era3";
+    }
+
+  fdEr = EvrOpen(&pEr, argv[1]);
+  if (fdEr == -1)
+    return errno;
+
+  if (argc > 2)
+    {
+      ch = atoi(argv[2]);
+    }
+  
+  if (argc > 3)
+    {
+      i = strtol(argv[3], NULL, 0);
+      i = EvrSetCMLMode(pEr, ch, i);
+    }
+  else
+    i = EvrSetCMLMode(pEr, ch, 0);
+
+  if (argc > 2)
+    printf("0x%04x\n", i);
+  
+  EvrClose(fdEr);
+
+  return 0;
+}
